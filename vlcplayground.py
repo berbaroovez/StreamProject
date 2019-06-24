8bc303ec1e2db6e55138a9015773f6e82d37b700
carterolson <carterolson3@gmail.com>
2019-06-23 23:59:32 -0500

    Got PyQt to be able to have a live stream play in the widget

--- vlcplayground.py
+++ vlcplayground.py
@@ -19,10 +19,10 @@ class App(QtWidgets.QMainWindow):
         self.instance = vlc.Instance()
 
         #Chooses the media that we want to play
-        self.media = self.instance.media_new("NickMercsThatsLit.mp4")
+        #self.media = self.instance.media_new()
         #Creates a mediaplyer and loads the selected media in
         self.mediaplayer = self.instance.media_player_new()
-        self.mediaplayer.set_media(self.media)
+        self.mediaplayer.set_mrl("https://video-weaver.ord02.hls.ttvnw.net/v1/playlist/Cr0DWQtBRBZWEZFtHY1AinsOtgn-9CXiT_k8ieqG33QHq0LHi6CwyCGi33l6gydA7BspRvkOvgf60wKKbQR0e_FHIv7ADzX7JSUrmWvl8dJ95IfnqUWUoxEbEIHbNj_er32zmyMdvqs0Qywd8plM2yfSOQohCm1XOoHlle4NuF_hI-TvvoS5IE-QEjddcV8-gEyXrOK7_zVZbDPufaESOG4_E9jhTCaWczCbm2nlzUwccmcdgwIWQ7nVNTAgMs1j0e_nloBxwASeRzpAN8sE_dzTlhJbH65iB-2FvDwwaMaY08pwhaAqB52I-NGXWfVboAG1TX6DV3_SUSDMJ0V-2qGUIbDrzCDXQpjB9Luk32znNCDUE-OmD6nMvzoC0UlMM0-57wvJ21ktz4zpfRTZirVrQxKnXxVssMFX2yLHgBZqZXqv2PFaagpSv1nhYSTb7I69fFCRB7WDG9Do9NK3onfqV08yZj78k09EW2ZGYfucyWVFYjFyK1HsJz2u97erRIYHwarbnX_zgZYuVPlYgvdaxTmKaVx5ZJOY8frUzJL_1N20W6sEfR7YeyxsSxa-b6ksGsNurpoKkfoSVTV62hIQq-s-qSUlC0Z0JRSDSaNBUBoMQIkcBsL4sxeebjZP.m3u8")
 
 
 
@@ -43,6 +43,7 @@ class App(QtWidgets.QMainWindow):
         self.videoframe.setAutoFillBackground(True)
 
 
+
     #Sets the layout of the video to a vertical layout
         self.vboxlayout = QtWidgets.QVBoxLayout()
         self.vboxlayout.addWidget(self.videoframe)
@@ -59,12 +60,13 @@ class App(QtWidgets.QMainWindow):
 
 
         #Plays the video
-        self.mediaplayer.play()
+      #  self.mediaplayer.play()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     ex = App()
     ex.show()
+    ex.mediaplayer.play()
     sys.exit(app.exec_())
 
     #
